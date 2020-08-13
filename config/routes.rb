5db3c3ff5e1ec71dbf4db4f9881b48b84bc00880
SWFullPotential <swfullpotential@gmail.com>
2020-08-12 19:18:23 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create '
     
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
